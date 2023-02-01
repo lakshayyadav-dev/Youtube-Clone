b287dce1c820eb5d01749e61179278dc20ed65e7
Lakshay <lakshayyadav2006@gmail.com>
2023-02-01 17:59:37 +0530

    created the sidebar.

--- youtube-clone/src/components/Feed.jsx
+++ youtube-clone/src/components/Feed.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
 
 const Feed = () => {
 	return (
-		<div>
-			Feed
-		</div>
+		<Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+			<Box
+				sx={{
+					height: { sx: "auto", md: "92vh" },
+					borderRight: "1px solid #1F1F1F",
+					px: { sx: 0, md: 2 },
+				}}
+			>
+                <Sidebar />
+
+                <Typography className="copyright" varient="body2" sx={{mt:1.5, color:"#fff"}}>
+                    Made by Lakshay Yadav
+                </Typography>
+            </Box>
+		</Stack>
 	);
 };
 
