d29a2f2d1e54fde5dc65f285be46b5c9c76221b7
Lakshay <lakshayyadav2006@gmail.com>
2023-02-02 23:41:34 +0530

    implemented the search and video detail features.

--- youtube-clone/src/components/SearchBar.jsx
+++ youtube-clone/src/components/SearchBar.jsx
@@ -5,10 +5,23 @@ import { Paper, IconButton } from "@mui/material";
 import { Search } from "@mui/icons-material";
 
 const SearchBar = () => {
+	const [searchTerm, setSearchTerm] = useState("");
+    const navigate = useNavigate();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+        if (searchTerm) {
+            navigate(`/search/${searchTerm}`);
+
+            setSearchTerm('');
+        }
+	};
+
 	return (
 		<Paper
 			component="form"
-			onSubmit={() => {}}
+			onSubmit={handleSubmit}
 			sx={{
 				borderRadius: 20,
 				border: "1px solid #e3e3e3",
@@ -17,10 +30,15 @@ const SearchBar = () => {
 				mr: { sm: 5 },
 			}}
 		>
-			<input className="search-bar" placeholder="Search..." value="" onChange={() => {}}/>
-            <IconButton type="submit" sx={{p:"10px", color: "red"}}>
-                <Search />
-            </IconButton>
+			<input
+				className="search-bar"
+				placeholder="Search..."
+				value={searchTerm}
+				onChange={(e) => setSearchTerm(e.target.value)}
+			/>
+			<IconButton type="submit" sx={{ p: "10px", color: "red" }}>
+				<Search />
+			</IconButton>
 		</Paper>
 	);
 };
--- youtube-clone/src/components/Videos.jsx
+++ youtube-clone/src/components/Videos.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Stack, Box } from "@mui/material";
 import { VideoCard, ChannelCard } from "./";
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+    if (!videos) return "Loading...";
+
 	return (
-		<Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+		<Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" gap={2}>
 			{videos.map((video, index) => (
 				<Box key={index}>
 					{video.id.channelId && <ChannelCard channelDetails={video} />}
